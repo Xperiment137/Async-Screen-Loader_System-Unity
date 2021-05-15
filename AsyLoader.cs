0023b674c326de5031a884fec707af35ff7f015d
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2021-05-15 21:19:15 +0200

    Add files via upload

---
+++ AsyLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class AsyLoader : MonoBehaviour
+{
+
+    private string Escena;
+    private int selector;
+    private string[] ArrayEscenas = new string[] { "Hospital", "CreacionPersonaje" ,"Cinematica1","Menu Principal", "Consciencia", "Casa","Pantano","Bahia","Pueblo"};
+
+
+    private void Awake()
+    {
+       
+        selector = PlayerPrefs.GetInt("Escena");
+        Escena = ArrayEscenas[selector];
+    }
+    void Start()
+    {
+        LoadButton();
+      
+    }
+
+    void LoadButton()
+    {
+        //Start loading the Scene asynchronously and output the progress bar
+        StartCoroutine(LoadScene());
+    }
+
+
+   
+        IEnumerator LoadScene()
+        {
+            yield return null;
+
+
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(Escena);
+
+
+
+            yield return null;
+        }
+    }
+    
---
+++ AsynHelp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsynHelp : MonoBehaviour
+{
+    private string Escena = "Pantalla de carga";
+  
+
+    void Start()
+    {
+        
+
+    }
+
+    public void LoadScene(int move)
+    {
+        PlayerPrefs.SetInt("Escena", move );
+        PlayerPrefs.Save();
+        StartCoroutine(LoadScene());
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(Escena);
+
+
+
+        yield return null;
+    }
+
+
+
+}
