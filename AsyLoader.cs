72e721b825afcc1bd75eb82a038b58d472f48b5c
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2021-05-15 21:21:55 +0200

    Update AsyLoader.cs

--- AsyLoader.cs
+++ AsyLoader.cs
@@ -8,13 +8,13 @@ public class AsyLoader : MonoBehaviour
 
     private string Escena;
     private int selector;
-    private string[] ArrayEscenas = new string[] { "Hospital", "CreacionPersonaje" ,"Cinematica1","Menu Principal", "Consciencia", "Casa","Pantano","Bahia","Pueblo"};
+    private string[] ArrayEscenas = new string[] { "Main Menu", "Scene 1" , "Scene 2"}; // name of your scenes
 
 
     private void Awake()
     {
        
-        selector = PlayerPrefs.GetInt("Escena");
+        selector = PlayerPrefs.GetInt("Escena"); // put this script in your Loading screen when it start it will load the next screen using the number of playerpref in the scene array 
         Escena = ArrayEscenas[selector];
     }
     void Start()
@@ -25,7 +25,7 @@ public class AsyLoader : MonoBehaviour
 
     void LoadButton()
     {
-        //Start loading the Scene asynchronously and output the progress bar
+        //Start loading the Scene asynchronously
         StartCoroutine(LoadScene());
     }
 
